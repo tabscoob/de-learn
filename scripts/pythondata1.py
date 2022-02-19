c8b4837d5b4109bdbb9880d5272462bd25ea6954
Cesar Mendez <me@csar.dev>
2022-02-19 12:49:21 -0600

    agregue funcion imprimir variable

---
+++ scripts/pythondata1.py
@@ -0,0 +1,15 @@
+
+# este funcion imprime un numero
+
+def propiedad_variable_integer(valor_numerico):
+    return print("el valor numerico es:",valor_numerico)
+
+propiedad_variable_integer(20)
+
+
+
+
+
+
+
+
